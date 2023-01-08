269ebcf063822c71a7c7f4a5705627ce58a43895
Ririe <nsaputri08@gmail.com>
2023-01-08 16:11:53 +0700

    create design hompage

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
-      initialRoute: Routes.WELCOME,
+      initialRoute: Routes.HOME,
       getPages: AppPages.routes,
     ),
   );
